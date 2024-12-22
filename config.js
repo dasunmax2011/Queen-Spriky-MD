efb8b84b967a9ad7ef45bec73b330aaa755be2c4
dasunmax2011 <dasunff550@gmail.com>
2024-12-22 18:26:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '62oSGK5T#Uj8W3V9xitQl2qBoJHD8gSWI0DIed0XCvsDjB2NFy_Y', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'HjRDmQ4Z#oou8SNvFkVrsTTxMBoy0WiNY-xfBIIAdUwACGhtMXCg', // Enter Your Session ID
     MONGODB: process.env.MONGODB || 'mongodb://mongo:aHCBTsUhzVIvadJNEBXYgzCePbwwkAhj@junction.proxy.rlwy.net:51068',// Enter Your MongoDB URL
     OWNER_NUMBER: process.env.OWNER_NUMBER || '94720244981',    // Enter Your Owner Number
 };
