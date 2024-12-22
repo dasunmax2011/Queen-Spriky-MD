27b533c373d30014141518342d42942234c154b4
dasunmax2011 <dasunff550@gmail.com>
2024-12-22 18:13:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    SESSION_ID: process.env.SESSION_ID || '62oSGK5T#Uj8W3V9xitQl2qBoJHD8gSWI0DIed0XCvsDjB2NFy_Y', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb://mongo:aHCBTsUhzVIvadJNEBXYgzCePbwwkAhj@junction.proxy.rlwy.net:51068',// Enter Your MongoDB URL
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '94720244981',    // Enter Your Owner Number
 };
